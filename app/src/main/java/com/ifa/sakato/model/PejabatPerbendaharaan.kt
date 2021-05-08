00d704be60871aa2e7a1513c6d5d2379a46b0f93
IFA <imamfh24@gmail.com>
2021-05-09 05:50:52 +0700

    PejabatPerbendaharaan Page

---
+++ app/src/main/java/com/ifa/sakato/model/PejabatPerbendaharaan.kt
@@ -0,0 +1,9 @@
+package com.ifa.sakato.model
+
+data class PejabatPerbendaharaan(
+    var namaLengkap: String,
+    var jabatan: String,
+    var email: String,
+    var noHP: String,
+    var jenisKelamin: String
+)
